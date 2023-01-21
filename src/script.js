32a137325ab07656895a61326e360fbdf9876cbc
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-01-20 19:25:50 -0500

    Formated time

--- src/script.js
+++ src/script.js
@@ -1,20 +1,7 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  let day_night = "AM";
-  if (hours > 12) {
-    day_night = "PM";
-    hours = hours - 12;
-  } else if (hours == 0) {
-    hours == 12;
-  }
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
   let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
   let days = [
     "Sunday",
     "Monday",
@@ -24,8 +11,17 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = date.getDay();
-  return `${day}, ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -35,12 +31,14 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let feelElement = document.querySelector("#feel");
+  let dateElement = document.querySelector("#time");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   feelElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "6d2f7310aaa7a0o47bf2b04d763t6f29";
