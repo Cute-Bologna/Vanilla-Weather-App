149d665668e76cbcab30b329dad77f98c34bee2d
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-01-22 13:32:09 -0500

    Temperature conversions, event listeners

--- src/script.js
+++ src/script.js
@@ -33,7 +33,10 @@ function displayTemperature(response) {
   let feelElement = document.querySelector("#feel");
   let dateElement = document.querySelector("#time");
   let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  
+  fahrenheitTemperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
@@ -60,8 +63,33 @@ function handleSubmit(event) {
 
 }
 
- search("New York");
+function displayCelsiusTemperature(event) {
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature");
+fahrenheitLink.classList.remove("active");
+celsiusLink.classList.add("active");
+let celsiusTemperature = (fahrenheitTemperature) - 32;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+
+}
+
+let fahrenheitTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link")
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+search("New York");
